1af2e0a8365a65a5eb3000056a98de138fbb05ae
xu <876133937@qq.com>
2017-11-06 11:29:24 +0800

    分页完成

---
+++ public/src/goodslist.js
@@ -0,0 +1,37 @@
+
+define(['jquery', 'template'], function ($, template) {
+
+    var size = 2;
+    
+    //利用正则匹配页码
+    var reg = /\?[a-z]+=(\d+)/;
+    //处理请求参数
+    var search = location.search || '';
+    //进行匹配查找
+    var page = reg.exec(search) && reg.exec(search)[1];
+    //设定默认页码
+    page = page || 1;
+
+    $.ajax({
+        url: '/api/product/queryProductDetailList',
+        type: 'get',
+        data: { page: page, pageSize: size },
+        success: function (info) {
+            // console.log(info);
+
+            //总条数
+            var total = info.total;
+            var pageLen = Math.ceil(total / size);
+            //调用模板引擎，处理商品列表
+            var html = template('tpl', info);
+            //调用模板引擎，处理分页
+            var pagehtml = template('page', {
+                pageLen: pageLen,
+                page:page
+            });
+            $('.goods').html(html);
+            $('.pagination').html(pagehtml);
+        }
+    })
+
+})
